2e7ef975ee78d552e40cf2ec96d07122b0d8cda2
paul-karas-nws <pkaras@normandiewebschool.fr>
2025-03-11 16:25:14 +0100

    Tailwind added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import tailwindcss from "@tailwindcss/vite";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [preact()]
+  site: "https://knyastro.netlify.app",
+  integrations: [preact()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
+
